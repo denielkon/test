27041269dd6f507ff1b54befd0a617a371657a99
denielkon <64771499+denielkon@users.noreply.github.com>
2021-03-16 13:36:45 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+$('.slider').slick({
+		
+		slidesToScroll: 1,
+		adaptiveHeight: true,
+		infinite: true,
+		centerMode: true,
+		slidesToShow: 1,
+	   dots: true,
+	   fade: true,
+	   arrows: false,
+  		cssEase: 'linear'	
+		
+ });
+});
